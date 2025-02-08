fc8450c7a2d5a62e2b50648d48dcfedc4479bf62
parshuram8884 <mohitparshuramthakur234@gmail.com>
2025-02-08 12:59:31 +0530

    fifty time

--- server/config/multer.js
+++ server/config/multer.js
@@ -4,7 +4,7 @@ const path = require('path');
 // Configure storage
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/food-images/');
+    cb(null, 'uploads/');
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
