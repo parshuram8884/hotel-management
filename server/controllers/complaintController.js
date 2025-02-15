0c9e1933bc473b14aa16b7964c848fd391de3cad
parshuram8884 <mohitparshuramthakur234@gmail.com>
2025-02-15 15:07:11 +0530

    fifty four

--- server/controllers/complaintController.js
+++ server/controllers/complaintController.js
@@ -22,118 +22,111 @@ const deleteOldComplaints = async () => {
 // Run cleanup every hour
 setInterval(deleteOldComplaints, 60 * 60 * 1000);
 
-const complaintController = {
-  // Get hotel complaints
-  getHotelComplaints: async (req, res) => {
-    try {
-      const complaints = await Complaint.find({ hotelId: req.user.hotelId });
-      res.json(complaints);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  },
-
-  // Get guest complaints
-  getGuestComplaints: async (req, res) => {
-    try {
-      const complaints = await Complaint.find({ guestId: req.user._id });
-      res.json(complaints);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  },
-
-  // Add predefined complaint
-  addPredefinedComplaint: async (req, res) => {
-    try {
-      const complaint = new Complaint(req.body);
-      await complaint.save();
-      res.status(201).json(complaint);
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
-  },
-
-  // Get predefined complaints for a hotel
-  getPredefinedComplaints: async (req, res) => {
-    try {
-      const complaints = await Complaint.find({ 
-        hotelId: req.params.hotelId,
-        isPredefined: true 
-      });
-      res.json(complaints);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  },
-
-  // Submit a complaint (both predefined and custom)
-  submitComplaint: async (req, res) => {
-    try {
-      const complaint = new Complaint({
-        ...req.body,
-        guestId: req.user._id
-      });
-      await complaint.save();
-      res.status(201).json(complaint);
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
-  },
-
-  // Update complaint status
-  updateComplaintStatus: async (req, res) => {
-    try {
-      const complaint = await Complaint.findByIdAndUpdate(
-        req.params.complaintId,
-        { status: req.body.status },
-        { new: true }
-      );
-      res.json(complaint);
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
-  },
-
-  // Add staff message
-  addStaffMessage: async (req, res) => {
-    try {
-      const complaint = await Complaint.findById(req.params.complaintId);
-      complaint.messages.push({
-        sender: 'staff',
-        content: req.body.message
-      });
-      await complaint.save();
-      res.json(complaint);
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
-  },
-
-  // Add guest message
-  addGuestMessage: async (req, res) => {
-    try {
-      const complaint = await Complaint.findById(req.params.complaintId);
-      complaint.messages.push({
-        sender: 'guest',
-        content: req.body.message
-      });
-      await complaint.save();
-      res.json(complaint);
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
-  },
-
-  // Delete complaint
-  deleteComplaint: async (req, res) => {
-    try {
-      await Complaint.findByIdAndDelete(req.params.complaintId);
-      res.status(200).json({ message: 'Complaint deleted successfully' });
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
+exports.getHotelComplaints = async (req, res) => {
+  try {
+    const complaints = await Complaint.find({ hotelId: req.user.hotelId });
+    res.json(complaints);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
 
-module.exports = complaintController;
+exports.getGuestComplaints = async (req, res) => {
+  try {
+    const complaints = await Complaint.find({ guestId: req.user._id });
+    res.json(complaints);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.addPredefinedComplaint = async (req, res) => {
+  try {
+    const complaint = new PredefinedComplaint(req.body);
+    await complaint.save();
+    res.status(201).json(complaint);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.getPredefinedComplaints = async (req, res) => {
+  try {
+    const complaints = await PredefinedComplaint.find({
+      hotelId: req.params.hotelId,
+      isActive: true
+    });
+    res.json(complaints);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.submitComplaint = async (req, res) => {
+  try {
+    const complaint = new Complaint({
+      ...req.body,
+      guestId: req.user._id
+    });
+    await complaint.save();
+    res.status(201).json(complaint);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.updateComplaintStatus = async (req, res) => {
+  try {
+    const complaint = await Complaint.findByIdAndUpdate(
+      req.params.complaintId,
+      { status: req.body.status },
+      { new: true }
+    );
+    res.json(complaint);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.addStaffMessage = async (req, res) => {
+  try {
+    const complaint = await Complaint.findById(req.params.complaintId);
+    if (!complaint) {
+      return res.status(404).json({ message: 'Complaint not found' });
+    }
+    complaint.messages.push({
+      message: req.body.message,
+      isStaff: true
+    });
+    await complaint.save();
+    res.json(complaint);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.addGuestMessage = async (req, res) => {
+  try {
+    const complaint = await Complaint.findById(req.params.complaintId);
+    if (!complaint) {
+      return res.status(404).json({ message: 'Complaint not found' });
+    }
+    complaint.messages.push({
+      message: req.body.message,
+      isStaff: false
+    });
+    await complaint.save();
+    res.json(complaint);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.deleteComplaint = async (req, res) => {
+  try {
+    await Complaint.findByIdAndDelete(req.params.complaintId);
+    res.status(200).json({ message: 'Complaint deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
