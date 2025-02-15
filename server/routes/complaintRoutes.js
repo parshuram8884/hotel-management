74cf1b98ffc1e6778abd853d380cc56dc1a9cdcd
parshuram8884 <mohitparshuramthakur234@gmail.com>
2025-02-15 15:16:19 +0530

    fifty six

--- server/routes/complaintRoutes.js
+++ server/routes/complaintRoutes.js
@@ -1,20 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const complaintController = require('../controllers/complaintController');
+const {
+    addPredefinedComplaint,
+    getHotelComplaints,
+    updateComplaintStatus,
+    addStaffMessage,
+    deleteComplaint,
+    getPredefinedComplaints,
+    submitComplaint,
+    getGuestComplaints,
+    addGuestMessage
+} = require('../controllers/complaintController');
 const { auth } = require('../middleware/auth');
 const guestAuth = require('../middleware/authMiddleware');
 
 // Staff routes
-router.post('/predefined', auth, complaintController.addPredefinedComplaint);
-router.get('/hotel', auth, complaintController.getHotelComplaints);
-router.patch('/:complaintId/status', auth, complaintController.updateComplaintStatus);
-router.post('/:complaintId/staff-messages', auth, complaintController.addStaffMessage);
-router.delete('/:complaintId', auth, complaintController.deleteComplaint);
+router.post('/predefined', auth, addPredefinedComplaint);
+router.get('/hotel', auth, getHotelComplaints);
+router.patch('/:complaintId/status', auth, updateComplaintStatus);
+router.post('/:complaintId/staff-messages', auth, addStaffMessage);
+router.delete('/:complaintId', auth, deleteComplaint);
 
 // Guest routes
-router.get('/predefined/:hotelId', complaintController.getPredefinedComplaints);
-router.post('/submit', guestAuth, complaintController.submitComplaint);
-router.get('/guest', guestAuth, complaintController.getGuestComplaints);
-router.post('/:complaintId/messages', guestAuth, complaintController.addGuestMessage);
+router.get('/predefined/:hotelId', getPredefinedComplaints);
+router.post('/submit', guestAuth, submitComplaint);
+router.get('/guest', guestAuth, getGuestComplaints);
+router.post('/:complaintId/messages', guestAuth, addGuestMessage);
 
 module.exports = router;
