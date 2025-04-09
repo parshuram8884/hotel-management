c9287523de12430c776718d04b2e0f60434aba19
parshuram8884 <106910128+parshuram8884@users.noreply.github.com>
2025-04-09 15:31:09 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,6 +6,8 @@ const authController = require('../controllers/authController');
 // Public routes
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password', authController.resetPassword);
 
 // Protected routes
 router.get('/settings/:hotelId', auth, authController.getSettings);
