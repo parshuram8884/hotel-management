fe382bcf5a5fc7abc93c39c6639688ebbc5d76c6
parshuram8884 <106910128+parshuram8884@users.noreply.github.com>
2025-07-21 17:21:37 +0530

    Update FoodManagement.jsx

--- client/src/pages/FoodManagement.jsx
+++ client/src/pages/FoodManagement.jsx
@@ -70,7 +70,7 @@ const FoodManagement = () => {
     try {
       const token = localStorage.getItem('token');
       await axios.post(
-        'import.meta.env.VITE_API_URL/api/food',
+        `${import.meta.env.VITE_API_URL}/api/food`,
         formDataToSend,
         {
           headers: { 
