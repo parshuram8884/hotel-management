cad12e61c0fadbf9034f19dd853c4f19c5964225
parshuram8884 <106910128+parshuram8884@users.noreply.github.com>
2025-07-21 17:23:12 +0530

    Update ComplaintManagement.jsx

--- client/src/pages/ComplaintManagement.jsx
+++ client/src/pages/ComplaintManagement.jsx
@@ -66,7 +66,7 @@ const ComplaintManagement = () => {
     try {
       const token = localStorage.getItem('token');
       await axios.post(
-        'import.meta.env.VITE_API_URL/api/complaints/predefined',
+        `${import.meta.env.VITE_API_URL}/api/complaints/predefined`,
         { title: newComplaint },
         {
           headers: { Authorization: token }
